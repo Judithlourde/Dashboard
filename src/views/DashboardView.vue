e2aaac03d639abdbef78508afe5c7d647ec36f7d
judith lourde <judith.lourde@gmail.com>
2021-12-09 22:30:52 +0100

    Added new views to the Components and deleted the older views(because it was wrong)

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,13 @@
+<template>
+    <DashboardHome />
+</template>
+
+<script>
+import DashboardHome from '../components/DashboardHome.vue';
+
+export default {
+    components: {
+        DashboardHome
+    }
+}
+</script>
---
+++ src/views/TodolistView.vue
@@ -0,0 +1,14 @@
+<template>
+    <Todolist :class="{todoView:!todoView}" />
+</template>
+
+<script>
+import Todolist from '../components/Todolist.vue';
+
+export default {
+    components: {
+        Todolist
+    }
+}
+</script>
+
