bb25936d508ae2454cac0bc3d66c0b23fdd395c3
judith lourde <judith.lourde@gmail.com>
2021-12-14 23:56:33 +0100

    Fixed svg path name

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -9,12 +9,12 @@
                 <input type="text" placeholder="+ Add a task and press Enter" v-model="todoEntered" @keyup.enter="addToList">
             </div>
 
-            <!-- Find the index helps for the slice() method -->
+            <!-- Find the index for the slice() method -->
             <div v-for="(todoItem, index) in todoItems" :key="todoItem.id" class="todo__list">
                 <div class="todo__content" v-if="!todoItem.taskStatus" >
                     <div>
                         <div @click="addToCompleted(todoItem.id, index)">
-                            <img src="../assets/svg/circlewithoutfill.svg" alt="circlewithoutfill-icon">
+                            <img src="/svg/circlewithoutfill.svg" alt="circlewithoutfill-icon">
                         </div>
 
                         <div class="todo__content-edit">
@@ -24,11 +24,11 @@
                         </div>
                     </div>
 
-                         <!-- <div class="list-container__todo--buttons"> -->
-                        <!-- editTodo(todoItem) - sending the object in parameter
-                        triggers the current object's details in the function -->
+                    <!-- <div class="list-container__todo--buttons"> -->
+                    <!-- editTodo(todoItem) - sending the object in parameter
+                    triggers the current object's details in the function -->
                     <div class="todo__edit" @click="editTodo(todoItem)">
-                        <img src="../assets/svg/edit.svg" alt="edit-icon">
+                        <img src="/svg/edit.svg" alt="edit-icon">
                     </div>
                 </div>
             </div>
@@ -39,13 +39,13 @@
                 <div v-for="(todoCompletedItem, index) in todoCompletedItems" :key="todoCompletedItem.id" class="todo__list">
                     <div class="todo__completed" v-if="!todoCompletedItem.taskStatus">
                         <div>
-                            <img src="../assets/svg/done.svg" alt="done-icon">
+                            <img src="/svg/done.svg" alt="done-icon">
 
                             <div>{{ todoCompletedItem.task }}</div>
                         </div>
 
                         <div class="todo__remove" @click="removeTodoItem(index)">
-                            <img src="../assets/svg/remove.svg" alt="remove-icon">
+                            <img src="/svg/remove.svg" alt="remove-icon">
                         </div>
                     </div>
                 </div>
@@ -55,82 +55,85 @@
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            todoView: false,
-            todoEntered: '',
-            edit: false,
-            todoItems: [],
-            todoCompletedItems: [],
-            status: false,
-            count: 0
-        };
-    },
+    export default {
+        data() {
+            return {
+                todoView: false,
+                todoEntered: '',
+                edit: false,
+                todoItems: [],
+                todoCompletedItems: [],
+                status: false,
+                count: 0
+            };
+        },
 
-    methods: {
-        addToList() {
-            // If condition helps to avoid empty data adds to the list
-            if (this.todoEntered === '') {
-                return
-            }
-            this.todoItems.push (
-                {
-                    id: this.id(),
-                    task: this.todoEntered,
-                    editing: this.edit,
-                    taskStatus: this.status,
-                    count: this.count
+        methods: {
+            addToList() {
+                // If condition helps to avoid empty data adds to the list
+                if (this.todoEntered === '') {
+                    return
                 }
-            );
-            this.todoEntered = ''
+
+                this.todoItems.push (
+                    {
+                        id: this.id(),
+                        task: this.todoEntered,
+                        editing: this.edit,
+                        taskStatus: this.status,
+                        count: this.count
+                    }
+                );
+                this.todoEntered = ''
+            },
+
+            id() {
+                return Math.random().toString(36).slice(2);
+            },
+
+            editTodo(todoItem) {
+                todoItem.editing = true
+            },
+
+            finishEdit(todoItem) {
+                todoItem.editing = false
+            },
+
+            removeTodoItem(index) {
+                this.todoCompletedItems.splice(index, 1);
+                this.count--
+            },
+
+            addToCompleted(id, index) {
+                const completedTask = this.todoItems.find(todoItem => todoItem.id === id);
+                    this.todoItems.splice(index, 1)
+
+                    this.todoCompletedItems.push( {
+                        task: completedTask.task,
+                        id: completedTask.id,
+                        count: completedTask.count
+                    });
+                    this.count++
+            },    
         },
 
-        id() {
-			return Math.random().toString(36).slice(2);
-		},
-
-        editTodo(todoItem) {
-            todoItem.editing = true
-        },
-
-        finishEdit(todoItem) {
-            todoItem.editing = false
-        },
-
-        removeTodoItem(index) {
-            this.todoCompletedItems.splice(index, 1);
-            this.count--
-        },
-
-        addToCompleted(id, index) {
-            const completedTask = this.todoItems.find(todoItem => todoItem.id === id);
-                this.todoItems.splice(index, 1)
-                this.todoCompletedItems.push( {
-                    task: completedTask.task,
-                    id: completedTask.id,
-                    count: completedTask.count
-                });
-                this.count++
-        },    
-    },
-
-    computed: {
-        stylingTodoView() {
-            return this.todoView
+        computed: {
+            stylingTodoView() {
+                return this.todoView
+            },
         }
     }
-}
 </script>
 
 <style>
     .todo {
-        max-height: 80vh; 
+        max-height: 80%;
     }
     
     .todo__header > h1 {
         padding: 20px;
         font-weight: 300;
+        font-size: 1em;
     }
 
     .todo__enterItems {
@@ -138,9 +141,8 @@ export default {
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
-        font-size: 1rem;
+        font-size: .75em;
     }
-
 
     .todo__enterItems > input {
         border: 1px solid #eee;
@@ -149,7 +151,6 @@ export default {
         height: 60px;
         font-size: inherit;
         font-family: inherit;
-
     }
 
     .todo__content, .todo__completed {
@@ -158,7 +159,6 @@ export default {
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
-
     }
 
     .todo__content > div, .todo__completed > div {
@@ -186,16 +186,22 @@ export default {
         width: 100%;
     }
 
+    /* styling todo view */
     .todoView {
-        height: 90%; 
-        border-radius: 15px; 
-        box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, .1); 
         background: #fff;
-        margin: 5vh;
+        margin: 50px;
         padding-right: 2%;
         padding-left: 2%;  
-        padding-top: 1%;  
-        font-size: 1.25rem;
-        
+        padding-top: 1%;
+        border-radius: 0.5em;
+        box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, .1);
+        height: 100%;     
+    }
+
+    /* Medium devices (landscape tablets, 768px and up) */
+    @media screen and (min-width: 768px) {
+        .todoView {
+            font-size: 1.5em;
+        }
     }
 </style>
